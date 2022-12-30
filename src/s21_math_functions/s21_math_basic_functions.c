43e65e7d0f6d189c2528ba3fb4b59d26db92bf91
Lorent Tressie <lorenttr@at-p4.msk.21-school.ru>
2022-12-31 00:16:27 +0300

    v2.0

--- src/s21_math_functions/s21_math_basic_functions.c
+++ src/s21_math_functions/s21_math_basic_functions.c
@@ -1,5 +1,7 @@
 #include "../s21_math.h"
 
+int s21_abs(int x) { return x & 1 << ((sizeof(int) * 8) - 1) ? -x : x; }
+
 long double s21_fabs(double x) {
   double_cast dc = {x};
   dc.parts.sgn = 0;
